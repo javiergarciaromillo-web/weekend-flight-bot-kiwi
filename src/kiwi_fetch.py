bd3d06737ac99cdb4bf92db4c95fdf0fe4c905ed
javiergarciaromillo-web <javiergarciaromillo@gmail.com>
2026-02-24 19:16:06 +0100

    Update kiwi_fetch.py

--- src/kiwi_fetch.py
+++ src/kiwi_fetch.py
@@ -2,12 +2,31 @@ import os
 import requests
 
 def test_call():
-    url = "REPLACE_WITH_RAPIDAPI_URL"
-    params = {}
-    headers = {
-        "X-RapidAPI-Key": os.environ["RAPIDAPI_KEY"],
-        "X-RapidAPI-Host": os.environ["RAPIDAPI_HOST"],
+    url = "https://kiwi-com-cheap-flights.p.rapidapi.com/round-trip"
+
+    querystring = {
+        "source": "City:amsterdam_nl",
+        "destination": "City:barcelona_es",
+        "currency": "EUR",
+        "locale": "en",
+        "adults": "1",
+        "children": "0",
+        "infants": "0",
+        "handbags": "1",
+        "holdbags": "0",
+        "cabinClass": "ECONOMY",
+        "sortBy": "QUALITY",
+        "sortOrder": "ASCENDING",
+        "transportTypes": "FLIGHT",
+        "limit": "5"
     }
-    r = requests.get(url, headers=headers, params=params, timeout=30)
-    r.raise_for_status()
-    return r.json()
+
+    headers = {
+        "x-rapidapi-key": os.environ["RAPIDAPI_KEY"],
+        "x-rapidapi-host": os.environ["RAPIDAPI_HOST"]
+    }
+
+    response = requests.get(url, headers=headers, params=querystring, timeout=30)
+    response.raise_for_status()
+
+    return response.json()
